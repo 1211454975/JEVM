78f86b5f91be1807faf403c55d92876d181b298d
DavePearce <david.pearce@ecs.vuw.ac.nz>
2017-12-21 14:45:26 +1300

    Fleshed out notion of contract a bit more.

--- src/jevm/core/Transaction.java
+++ src/jevm/core/Transaction.java
@@ -7,25 +7,79 @@ import static jevm.util.Number.*;
  * @author David J. Pearce
  *
  */
-public class Transaction {
+public abstract class Transaction {
 	/**
-	 * A scalar value equal to the number of transactions sent by the sender;
-	 * formally T_n.
+	 * Records the number of transactions sent by the sender.
 	 */
-	private u256 nonce;
+	private final u256 nonce;
 	/**
-	 * A scalar value equal to the number of Wei to be paid per unit of gas for all
-	 * computa- tion costs incurred as a result of the execution of this
-	 * transaction; formally T_p.
+	 * Determines the number of Wei to be paid per unit of gas for all
+	 * computation costs incurred from executing this transaction.
 	 */
-	private u256 gasPrice;
+	private final u256 gasPrice;
 	/**
-	 * A scalar value equal to the maximum amount of gas that should be used in
-	 * executing this transaction. This is paid up-front, before any computation is
-	 * done and may not be increased later; formally T_g.
+	 * Determines the maximum amount of gas available for executing this
+	 * transaction. This is paid before any computation is done and may not be
+	 * increased later.
 	 */
-	private u256 gasLimit;
-	private u160 to;
-	private u256 value;
-	private u256 v, r, s;
+	private final u256 gasLimit;
+	/**
+	 * The 160-bit address of the message call’s recipient or, for a contract
+	 * creation transaction ?????.
+	 */
+	private final u160 to;
+	/**
+	 * Determines the amount of Wei to be transferred to the message's recipient or,
+	 * in the case of contract creation, as an endowment to the newly created
+	 * account.
+	 */
+	private final u256 value;
+
+	private final u256 v, r, s;
+
+	public Transaction(u256 nonce, u256 gasPrice, u256 gasLimit, u160 to, u256 value, u256 v, u256 r, u256 s) {
+		this.nonce = nonce;
+		this.gasPrice = gasPrice;
+		this.gasLimit = gasLimit;
+		this.to = to;
+		this.value = value;
+		this.v = v;
+		this.r = r;
+		this.s = s;
+	}
+
+	/**
+	 * Represents the creation of a new account.
+	 *
+	 * @author David J. Pearce
+	 *
+	 */
+	public static final class AccountCreation extends Transaction {
+		/**
+		 * A code fragment which returns the body to be associated with the newly
+		 * created account. The body is another code fragment which executes every time
+		 * the account receives a message call. The init code is executed exactly once
+		 * at account creation time.
+		 */
+		private final Bytecode[] init;
+
+		public AccountCreation(u256 nonce, u256 gasPrice, u256 gasLimit, u160 to, u256 value, u256 v, u256 r, u256 s, Bytecode[] init) {
+			super(nonce,gasPrice,gasLimit,to,value,v,r,s);
+			this.init = init;
+		}
+	}
+
+	public static final class MessageCall extends Transaction {
+		/**
+		 * An unlimited size byte array which determines the input data for the message
+		 * call.
+		 */
+		private final byte[] data;
+
+		public MessageCall(u256 nonce, u256 gasPrice, u256 gasLimit, u160 to, u256 value, u256 v, u256 r, u256 s,
+				byte[] data) {
+			super(nonce, gasPrice, gasLimit, to, value, v, r, s);
+			this.data = data;
+		}
+	}
 }
